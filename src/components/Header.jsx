c07c6ea83f03deda63d07c468d537174760773bd
iman <iman.bachatt@gmail.com>
2023-03-06 22:47:43 +0200

    lastchange

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,49 +4,43 @@ import './Header.css'
 const Header = () => {
   return (
     <>
-      <div className='nav snip1226'>
-
-        <input type='checkbox' id='checkbox_toggle' />
-
-        <label for='checkbox_toggle' class='hamburger'>
-          &#9776;
-        </label>
-        <ul className='snip1226 menu '>
-
-          <li className='logoo'>
-            {' '}
-            <img
-              src={require('../media/exotic-places-logo-removebg-preview.png')}
-              alt=''
-              className='logo'
-            />
-          </li>
-
-          <li className='current'>
-            <a href='#home' data-hover='Home'>
-              Home
-            </a>
-          </li>
-          <li>
-            <a href='#PlacesToGo' data-hover='Place To Go'>
-              Place To Go
-            </a>
-          </li>
-          <li>
-            <a href='#ThingsToDo' data-hover='Things To Do'>
-              Things To Do
-            </a>
-          </li>
-          <li>
-            <a href='#BookYourTrip' data-hover='Book Your Trip'>
-              Book Your Trip
-            </a>
-          </li>
-
-        </ul>
-      </div>
-
-
+      <nav class="navbar ">
+        <div class="left">
+          <h1>  <img
+            src={require('../media/exotic-places-logo-removebg-preview.png')}
+            alt=''
+            className='logo'
+          /></h1>
+        </div>
+        <div class="right">
+          <input type="checkbox" id="check" />
+          <label for="check" class="checkBtn">
+            <i class="fa fa-bars"></i>
+          </label>
+          <ul class="list">
+            <li>
+              <a href='#home' data-hover='Home'>
+                Home
+              </a>
+            </li>
+            <li>
+              <a href='#PlacesToGo' data-hover='Place To Go'>
+                Place To Go
+              </a>
+            </li>
+            <li>
+              <a href='#ThingsToDo' data-hover='Things To Do'>
+                Things To Do
+              </a>
+            </li>
+            <li>
+              <a href='#BookYourTrip' data-hover='Book Your Trip'>
+                Book Your Trip
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
     </>
   )
 }
